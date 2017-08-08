c4a405437f3af71a345d958b93020cd1db33a8d2
Pablo Arreche <pablo.arreche@gmail.com>
2017-08-08 08:55:59 -0300

    Fixed error 500 on grids

--- src/main/java/com/sample/highcharts/config/WebMvpInitializer.java
+++ src/main/java/com/sample/highcharts/config/WebMvpInitializer.java
@@ -1,19 +1,27 @@
 package com.sample.highcharts.config;
 
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
 public class WebMvpInitializer implements WebApplicationInitializer {
   @Override
-  public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
-    AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-    rootContext.register(SpringConfig.class);
-    ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher", new DispatcherServlet(rootContext));
-    registration.addMapping("/");
-    registration.setLoadOnStartup(1);
+  public void onStartup(ServletContext ctx) throws ServletException {
+	  AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+      
+      // Manage the lifecycle of the root application context
+      ctx.addListener(new ContextLoaderListener(rootContext));
+	  
+      AnnotationConfigWebApplicationContext webCtx = new AnnotationConfigWebApplicationContext();
+      webCtx.register(SpringConfig.class);
+      webCtx.setServletContext(ctx);
+      ServletRegistration.Dynamic servlet = ctx.addServlet("dispatcher", new DispatcherServlet(webCtx));
+      servlet.setLoadOnStartup(1);
+      servlet.addMapping("/");
   }
 }
